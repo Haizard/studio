a20719448a5397b6776cadb1a063d9d79b0113e4
haitham misape <haithammisape@gmail.com>
2025-07-15 16:26:51 +0000

    1 of 1 error Next.js (14.2.4) is outdated (learn more) Unhandled Runtime

---
+++ src/next.config.js
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    newNextLinkBehavior: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
